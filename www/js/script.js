2eb46d8ab46770969557f11751e5d9520a865ce3
ontariokwasi <ontariokwasi@gmail.com>
2016-09-08 03:49:03 +0000

    Add files via upload

---
+++ www/js/script.js
@@ -0,0 +1,6 @@
+$(document).on("pagecreate", function() {
+	$("#splahLabel1").slideDown(3000, function() {
+		window.location = 'login.html';
+		// window.location = 'http://216.224.161.207/nalocollector/login.php';
+	});
+});
